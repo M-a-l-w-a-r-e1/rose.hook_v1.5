3cc8eb27b863f292c9962bca51bd73060b0788d0
rose-is-gay <71583406+rose-is-gay@users.noreply.github.com>
2022-05-30 22:30:44 -0400

    Add project files.

---
+++ chesat/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░");
+Console.WriteLine("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░   ░░░░░");
+Console.WriteLine("▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒");
+Console.WriteLine("▒  ▒    ▒▒▒▒   ▒▒▒▒▒▒     ▒▒▒▒▒   ▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒▒   ▒▒▒▒▒▒▒▒   ▒▒▒▒▒   ▒▒   ");
+Console.WriteLine("▓▓   ▓▓▓▓▓   ▓▓   ▓▓   ▓▓▓▓▓▓  ▓▓▓   ▓▓▓▓▓▓     ▓▓▓▓▓   ▓▓   ▓▓▓   ▓▓   ▓▓   ▓   ▓");
+Console.WriteLine("▓▓   ▓▓▓▓   ▓▓▓▓   ▓▓▓    ▓▓         ▓▓▓▓▓▓   ▓▓  ▓▓   ▓▓▓▓   ▓   ▓▓▓▓   ▓     ▓▓▓");
+Console.WriteLine("▓▓   ▓▓▓▓▓   ▓▓   ▓▓▓▓▓▓   ▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓   ▓▓   ▓▓   ▓▓▓   ▓▓   ▓▓   ▓   ▓");
+Console.WriteLine("█    ███████   █████      ████     ████  ██  ███   ████   ████████   █████   ██   ");
+Console.WriteLine("██████████████████████████████████████████████████████████████████████████████████");
+Console.ForegroundColor = ConsoleColor.White;
+
+    Console.Title = "rOsE.hOoK";
+    Console.CursorVisible = false;
+    Console.SetCursorPosition(1, 1);
+
+    for (int i = 0; i < 50; i++)
+    {
+        for (int y = 0; y < i; y++)
+        {
+            string pb = "\u2551";
+            Console.WriteLine(pb);
+        }
+        Console.Write(i + " /50");
+        Console.SetCursorPosition(1 ,1);
+        System.Threading.Thread.Sleep(50);
+    }
+Console.ReadLine();
+
+
